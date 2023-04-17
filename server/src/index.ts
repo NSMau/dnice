b6ee758b658471b655495b0e3e99c70aeba3198e
mprcodes <mauriciopaternina@icloud.com>
2023-04-17 12:27:17 -0500

    set up expressMiddleware function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,43 @@
-// import express from 'express'
+import express from 'express'
 
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4'
+
+import { json } from 'body-parser'
+import cors from 'cors'
+import http from 'http'
 
 import { typeDefs, resolvers } from './schema'
 
-// const app = express()
+const app = express()
+const PORT = 4000
+
+// Tell Apollo Server to "drain" this httpServer,
+// enabling our server to shut down gracefully
+const httpServer = http.createServer(app)
 
 // Apollo creates the schema behind the scenes
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 })
 
-// async IIFE
+// Ensure we await for the server to start
 ;(async () => {
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 }
-  })
+  await server.start()
 
-  console.log(`🚀  Server ready at: ${url}`)
+  // Set up our Express middleware to handle CORS, body parsing, and
+  // our expressMiddleware function from Apollo Server
+  app.use('/api', cors<cors.CorsRequest>(), json(), expressMiddleware(server))
+})()
 
-  return url
+// Start the server
+;(async () => {
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  )
+
+  console.log(`🚀  Server ready at: http://localhost:${PORT}`)
 })()
